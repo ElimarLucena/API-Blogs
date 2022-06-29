64e06e91624f7c383146089f46bb6ab395f4fd7a
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-29 20:20:49 -0300

    feat: create  file

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,14 +1,9 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable import/no-dynamic-require */
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-// eslint-disable-next-line import/no-dynamic-require
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -20,16 +15,15 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => {
+  .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach((file) => {
-    // eslint-disable-next-line global-require
+  .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
