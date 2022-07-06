d873f62815888e1aa1e7daefb6d85d93b8e6680f
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-06 08:15:56 -0300

    feat: creating model and migrate for blogPost

---
+++ src/database/migrations/20220706110901-create-blog-post.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      published: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('BlogPosts');
+  },
+};
---
+++ src/database/models/blogPost.js
@@ -0,0 +1,31 @@
+const BlogPostSchema = (sequelize, DataTypes) => {
+  const BlogPostTable = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+    updated: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+  }, { timestamps: false });
+
+  BlogPostTable.associate = (models) => {
+    BlogPostTable.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPostTable;
+};
+
+module.exports = BlogPostSchema;
