ca1bd92de7dfa35752ed8b370e2e491720f5ffc6
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-06 08:07:42 -0300

    feat: creating model and migrate for category

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+const CategorySchema = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return CategoryTable;
+};
+
+module.exports = CategorySchema;
