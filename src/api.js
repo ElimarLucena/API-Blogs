7e80de3e004ba5b687325268c8942fe8f9094154
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-10 15:18:38 -0300

    fix:bug: correcting import routes

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routers');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
