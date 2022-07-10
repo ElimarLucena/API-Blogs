d896f29739611b8b54e07f5cdbbcd16a2fadec1f
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-10 15:29:52 -0300

    feat: authentication function the function does a search for the user and returns their access token

---
+++ src/services/loginService.js
@@ -0,0 +1,21 @@
+const { User } = require('../database/models');
+const generateJWTToken = require('../utils/jwt.js');
+
+const authenticate = async (payload) => {
+  const { email, password } = payload;
+
+  const user = await User.findOne({
+    attributes: ['id', 'displayName', 'email', 'image'],
+    where: { email, password },
+  });
+
+  if (!user) return false;
+
+  const token = generateJWTToken(user.dataValues);
+
+  return { token };
+};
+
+module.exports = {
+  authenticate,
+};
