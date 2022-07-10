440e67e86710a629c0b8caf8529f6b4f4c29f29d
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-10 15:22:59 -0300

    feat: user authentication function

---
+++ src/controllers/loginController.js
@@ -0,0 +1,15 @@
+const services = require('../services/loginService');
+
+const authenticate = async (req, res) => {
+  const token = await services.authenticate(req.body);
+
+  if (!token) {
+    return res.status(400).json({ message: 'Invalid fields' });
+  }
+
+  return res.status(200).json(token);
+};
+
+module.exports = {
+  authenticate,
+};
