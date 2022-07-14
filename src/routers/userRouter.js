aaf0518d95c2077b5c287fd25b62db2239fd44ac
Elimar Lucena <elimarlucenar@gmail.com>
2022-07-14 08:42:17 -0300

    feat: applying validation middleware to user route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controllers = require('../controllers/userController');
+const { userCreationValidation } = require('../middleware/userMiddleware');
 
 const userRouter = express.Router();
 
-userRouter.post('/', controllers.createUser);
+userRouter.post('/', userCreationValidation, controllers.createUser);
 
 module.exports = userRouter;
